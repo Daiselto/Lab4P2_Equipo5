dfbb176c12858436d7cfc44848fc268c328693de
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-08-11 14:38:09 -0600

    Clase Entrenador y Pokemon

--- src/lab4p2_equipo5/Entrenador.java
+++ src/lab4p2_equipo5/Entrenador.java
@@ -4,10 +4,91 @@
  */
 package lab4p2_equipo5;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author HP
  */
 public class Entrenador {
+    private String Nombre;
+    private int Edad, Dinero;
+    private Pokemon[] Team= new Pokemon[5];
+    private ArrayList<Pokemon> PC = new ArrayList();
+
+    public Entrenador() {
+    }
+
+    public Entrenador(String Nombre, int Edad, int Dinero) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Dinero = Dinero;
+    }
+    
+    public Entrenador(String Nombre, int Edad, int Dinero, Pokemon[] Team, ArrayList<Pokemon> PC) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Dinero = Dinero;
+        this.Team=Team;
+        this.PC=PC;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getDinero() {
+        return Dinero;
+    }
+
+    public void setDinero(int Dinero) {
+        this.Dinero = Dinero;
+    }
+
+    public Pokemon[] getTeam() {
+        return Team;
+    }
+
+    public void setTeam(Pokemon[] Team) {
+        this.Team = Team;
+    }
+
+    public ArrayList<Pokemon> getPC() {
+        return PC;
+    }
+
+    public void setPC(ArrayList<Pokemon> PC) {
+        this.PC = PC;
+    }
+    
+    public void PokemonalPC(Pokemon Pokemon){
+        PC.add(Pokemon);
+    }
+    
+    /*public void PokemonalTeam(Pokemon Pokemon){
+        for (int i = 0; i < Team.length ; i++) {
+            if () {
+                
+            }
+        }
+    }*/
+
+    @Override
+    public String toString() {
+        return "Entrenador{" + "Nombre=" + Nombre + ", Edad=" + Edad + ", Dinero=" + Dinero + ", Team=" + Team + ", PC=" + PC + '}';
+    }
+    
     
 }
